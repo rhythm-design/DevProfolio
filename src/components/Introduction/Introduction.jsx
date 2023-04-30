91276c06d0451398cf172a000d63807a79cd38c3
Rhythm <rhythmrv3@gmail.com>
2023-04-30 23:28:29 +0530

    Adding Introduction component

---
+++ src/components/Introduction/Introduction.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Introduction = () => {
+
+    return (
+
+        <div>
+            <p> 
+                Hi, I am Rhythm Varshney. 
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Odit ullam dicta quae illum doloremque unde iure. 
+                Eum nam dolorem repudiandae adipisci, 
+                quis optio sapiente fugit non. Ex rem quaerat animi!
+            </p>
+            
+        </div>
+    );
+
+}
+
+export default Introduction
+
+;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Introduction from "./Introduction/Introduction";
+const Portfolio = () =>{
+    return (
+        <div>
+            <Introduction />
+        </div>
+    );
+}
+
+export default Portfolio;
