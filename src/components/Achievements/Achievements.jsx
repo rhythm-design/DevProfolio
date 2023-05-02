34bc6dd15d0d8ac94fde99615b931a520bda74b0
Rhythm <rhythmrv3@gmail.com>
2023-05-02 17:30:01 +0530

    Adding Achievements Component and solving Not found issue

---
+++ src/components/Achievements/Achievements.jsx
@@ -0,0 +1,30 @@
+
+const Achievements = () => {
+    return (
+        <div>
+            <h1> 
+                Achievements and Certifications
+            </h1>
+
+            <ul>
+                <li>
+                    1 goes here
+                </li>
+                <li>
+                    Lorem, ipsum dolor sit amet consectetur 
+                    adipisicing elit. Quae, sapiente officia 
+                </li>
+                <li>
+                    consectetur eum sed expedita velit labore
+                    suscipit fugit maxime laborum quas odit 
+                    ad sint itaque! Amet sint numquam tempora?
+                </li>
+                <li>
+                    Lorem, ipsum dolor sit amet consectetur 
+                    adipisicing elit. Quae, sapiente officia
+                </li>
+            </ul>
+        </div>
+    );
+}
+
