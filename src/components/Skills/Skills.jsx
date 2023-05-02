66097b0f9a8148713a19368dc5c9f4f83149a319
Rhythm <rhythmrv3@gmail.com>
2023-05-02 15:50:39 +0530

    Updating skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -11,6 +11,20 @@ const Skills = () =>{
                 FULL STACK DEVELOPER WHO WANT TO EXPLORE AND BECOME 
                 BEST IN EVERY TECH STACK
             </h3>
+
+            <ul>
+                <li>
+                    Develop highly interactive FrontEnd / User Interface for 
+                    you web and mobile applications.
+                </li>
+                <li>
+                    Design time and space efficient algorithms to improve
+                    the application performance for best user Experience.
+                </li>
+                <li>
+                    Integeration of third party services such as Firebase
+                </li>
+            </ul>
         </div>
     );
 }
