7586cd192727f909b3a25d9dcf96e2feaffc721e
Rhythm <rhythmrv3@gmail.com>
2023-05-02 17:25:53 +0530

    Adding Proficiency component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Introduction from "./Introduction/Introduction";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
+import Proficiency from "./Proficiency/Proficiency";
+import Achievements from "./Achievements/Achievements";
 const Portfolio = () =>{
     return (
         <div>
             <Introduction />
             <Skills />
             <Projects />
+            <Proficiency />
+            <Achievements />
         </div>
     );
 }
---
+++ src/components/Proficiency/Proficiency.jsx
@@ -0,0 +1,18 @@
+
+const Proficiency = () => {
+    return (
+        <div>
+            <h1>
+                Proficiency
+            </h1>
+            <ul>
+                <li>FrontEnd Development</li>
+                <li>Backend Development</li>
+                <li>Springboot</li>
+                <li>Data Structures and Algorithms</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Proficiency;
