dd8e24c890663ce0e14f0064df282a79b6362f23
Rhythm <rhythmrv3@gmail.com>
2023-05-05 22:44:07 +0530

    Adding SASS dependency for SCSS compiling to CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Portfolio from './components/Portfolio';
 
 function App() {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./Navbar.css"
+import "./Navbar.scss"
 
 const Navbar = () => {
     return (
