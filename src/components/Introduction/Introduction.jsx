b6558a132ae061fd9adbb1a2d270d694baa03b5a
Rhythm <rhythmrv3@gmail.com>
2023-05-10 01:32:19 +0530

    Added Top animation to Introduction component
    Using react-awesome-reveal

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -10,23 +10,29 @@ const Introduction = () => {
 
     return (
 
-        <div>
-            <h1>
-                Hi, I am Rhythm Varshney. 
-            </h1>
-            <p> 
-                A passionate Full Stack Developer. I have built projects 
-                that showcase my skills, and I love learning and building 
-                new and innovative technology. I have build applications 
-                with JavaScript, ReactJS, NodeJS, SpringBoot and some other 
-                cool libraries and frameworks. I can design efficient 
-                algorithms which can help in improving the performance
-                of the application.
-            </p>
-            <div className="man-hello" >
-                <RenderLottie animationJSON={ManHello}/>
+        <Fade direction="up" duration={2000} distance="30px">
+            <div className="introduction-container">
+                <div className="half-box">
+                    <h1>
+                        Hi, I am Rhythm Varshney. 
+                    </h1>
+                    <p> 
+                        A passionate Full Stack Developer. I have built projects 
+                        that showcase my skills, and I love learning and building 
+                        new and innovative technology. I have build applications 
+                        with JavaScript, ReactJS, NodeJS, SpringBoot and some other 
+                        cool libraries and frameworks. I can design efficient 
+                        algorithms which can help in improving the performance
+                        of the application.
+                    </p>
+                </div>
+                <div className="half-box" >
+                    <div className="man-hello">
+                        <RenderLottie className="man-hello" animationJSON={ManHello}/>
+                    </div>  
+                </div>
             </div>
-        </div>
+        </Fade>
     );
 
 }
