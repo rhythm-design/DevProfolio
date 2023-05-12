43b3b5669ae4fafc32c93717246bf330c08c8309
Rhythm <rhythmrv3@gmail.com>
2023-05-12 22:28:19 +0530

    Adding Styles to Skills component
    Added lottie and awesome SCSS view for the component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,30 +1,39 @@
 import React from "react";
+import emoji from "react-easy-emoji";
+
+import ManProgramming from "../../assets/lottie/man-programming.json";
+import RenderLottie from "../RenderLottie/RenderLottie";
+import "./Skills.scss";
 
 const Skills = () =>{
     
     return (
-        <div>
-            <h1>
-                What I do
-            </h1>
-            <h3>
-                FULL STACK DEVELOPER WHO WANT TO EXPLORE AND BECOME 
-                BEST IN EVERY TECH STACK
-            </h3>
+        <div className="skills-container">
+            <div className="skills-lottie-anim">
+                <RenderLottie animationJSON={ManProgramming} />
+            </div>
+            <div className="skills-text-container">
+                <h1 className="skills-heading">
+                    What I do
+                </h1>
+                <h3 className="skills-subheading">
+                    FULL STACK DEVELOPER WHO WANT TO EXPLORE AND BECOME 
+                    BEST IN EVERY TECH STACK
+                </h3>
 
-            <ul>
-                <li>
-                    Develop highly interactive FrontEnd / User Interface for 
-                    you web and mobile applications.
-                </li>
-                <li>
-                    Design time and space efficient algorithms to improve
-                    the application performance for best user Experience.
-                </li>
-                <li>
-                    Integeration of third party services such as Firebase
-                </li>
-            </ul>
+                <ul className="skills-list">
+                    <li>
+                        {emoji("⚡ Develop highly interactive FrontEnd / User Interface for you web and mobile applications.")}
+                    </li>
+                    <li>
+                        {emoji("⚡ Design time and space efficient algorithms to improve the application performance for best user Experience.")} 
+                    </li>
+                    <li>
+                        {emoji("⚡ Integeration of third party services such as Firebase")}
+                        
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
