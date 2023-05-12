8a752244870803eaffb260ddc911211df51acaba
Rhythm <rhythmrv3@gmail.com>
2023-05-13 00:06:06 +0530

    Adding SoftwareSkills component for skill icons
    FontAwesome icons forskills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,6 +3,7 @@ import emoji from "react-easy-emoji";
 
 import ManProgramming from "../../assets/lottie/man-programming.json";
 import RenderLottie from "../RenderLottie/RenderLottie";
+import SoftwareSkills from "../SoftwareSkills/SoftwareSkills";
 import "./Skills.scss";
 
 const Skills = () =>{
@@ -20,6 +21,8 @@ const Skills = () =>{
                     FULL STACK DEVELOPER WHO WANT TO EXPLORE AND BECOME 
                     BEST IN EVERY TECH STACK
                 </h3>
+
+                <SoftwareSkills />
 
                 <ul className="skills-list">
                     <li>
---
+++ src/components/SoftwareSkills/SoftwareSkills.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const SoftwareSkills = () => {
+    return (
+        <div className="software-skills-container">
+            <ul className="dev-icons">
+                <li className="software-skill-inline">
+                    <i className="fab fa-html5"></i>
+                    <p>HTML-5</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-css3-alt"></i>
+                    <p>CSS3</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-sass"></i>
+                    <p>sass</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-java"></i>
+                    <p>Java</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fas fa-code"></i>
+                    <p>DSA</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-js"></i>
+                    <p>JavaScript</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-react"></i>
+                    <p>ReactJS</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-node"></i>
+                    <p>NodeJS</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fas fa-database"></i>
+                    <p>Database</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-java"></i>
+                    <p>SpringBoot</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fab fa-aws"></i>
+                    <p>AWS (Basic)</p>
+                </li>
+                <li className="software-skill-inline">
+                    <i className="fas fa-bug"></i>
+                    <p>Unit Testing</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default SoftwareSkills;
