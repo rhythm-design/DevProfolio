e95e2bf77be1352a0e2193dd42d5c8a4bfd945e8
Rhythm <rhythmrv3@gmail.com>
2023-05-15 01:14:26 +0530

    Adding TechProgress component
    Added CSS for Proficiency component and added TechProgress component for skill progress showcase

---
+++ src/components/TechProgress/TechProgress.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const TechProgress = () => {
+    return (
+        <div className="skill-bars-container">
+            <div className="skills-bar">
+                <h1>
+                        Proficiency
+                </h1>
+                <div className="skill frontend">
+                    <p>Frontend</p>
+                    <div className="meter">
+                        <span style={{width: '80%'}}></span>
+                    </div>
+                </div>
+                <div className="skill backend">
+                    <p>Backend</p>
+                    <div className="meter">
+                        <span style={{width: '60%'}}></span>
+                    </div>
+                </div>
+                <div className="skill java-springboot">
+                    <p>Java and SpringBoot</p>
+                    <div className="meter">
+                        <span style={{width: '80%'}}></span>
+                    </div>
+                </div>
+                <div className="skill programming">
+                    <p>Programming and DSA</p>
+                    <div className="meter">
+                        <span style={{width: '90%'}}></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TechProgress;
--- src/containers/Proficiency/Proficiency.jsx
+++ src/containers/Proficiency/Proficiency.jsx
@@ -1,18 +1,18 @@
 import RenderLottie from "../../components/RenderLottie/RenderLottie";
 import ProficientLottie from "../../assets/lottie/Proficiency-lottie.json";
+import TechProgress from "../../components/TechProgress/TechProgress";
+import "./Proficiency.scss"
 const Proficiency = () => {
     return (
-        <div>
-            <h1>
-                Proficiency
-            </h1>
-            <ul>
-                <li>FrontEnd Development</li>
-                <li>Backend Development</li>
-                <li>Springboot</li>
-                <li>Data Structures and Algorithms</li>
-            </ul>
-            <RenderLottie animationJSON={ProficientLottie} />
+        <div className="prof-container">
+            <div className="prof-text">
+                <TechProgress />
+            </div>
+            <div className="prof-lottie">
+                <div className="lottie-wrapper">
+                    <RenderLottie animationJSON={ProficientLottie} />
+                </div>
+            </div>
         </div>
     );
 }
